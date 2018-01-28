b4f6facbe5d2189ce5e98a8fca22f062781f1418
alex <alex.qiubo@qq.com>
2018-01-28 12:47:21 +0800

    modify

--- protected/controller/mobile/general_controller.php
+++ protected/controller/mobile/general_controller.php
@@ -18,7 +18,7 @@ class general_controller extends Controller
         {
             $realUrl = $_SERVER['HTTP_HOST'];
             $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$GLOBALS['wechat']['AppID'].'&redirect_uri='.urlencode($realUrl).'&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect';
-            log::write($realUrl);
+            log::write($url);
             return redirect($url);
         }else{
             return true;
