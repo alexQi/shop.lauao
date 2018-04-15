8d03c2d53dd344087bd07cace1bc9d08a71ae729
alex <alex.qiubo@qq.com>
2018-04-15 17:27:14 +0800

    modify

--- protected/config.php
+++ protected/config.php
@@ -5,11 +5,11 @@ return array
 (
     'mysql' => array
     (
-        'MYSQL_HOST' => 'localhost',
+        'MYSQL_HOST' => 'www.ccuui.com',
         'MYSQL_PORT' => '3306',
         'MYSQL_USER' => 'root',
-        'MYSQL_DB'   => 'akmall',
-        'MYSQL_PASS' => 'woshishei',
+        'MYSQL_DB'   => 'morechic',
+        'MYSQL_PASS' => 'woshishei@1',
         'MYSQL_DB_TABLE_PRE' => 'akmall_',
         'MYSQL_CHARSET' => 'UTF8',
     ),
